ef3fb66bab9bf0b72ed9591d3d6356d8844d1461
Dmitrii Bogdanov <dimyriy.bogdanov@gmail.com>
2018-07-22 14:49:05 +0200

    Implement InsertionSort

---
+++ src/main/java/org/dimyriy/algorithms/sort/InsertionSort.java
@@ -0,0 +1,18 @@
+package org.dimyriy.algorithms.sort;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author Dmitrii Bogdanov
+ * Created at 22.07.18
+ */
+public class InsertionSort<T extends Comparable<T>> extends AbstractSort<T> {
+  @Override
+  void sortImpl(@Nonnull final T[] arr) {
+    for (int i = 1; i < arr.length; i++) {
+      for (int j = i; j > 0 && arr[j - 1].compareTo(arr[j]) > 0; j--) {
+        swap(arr, j, j - 1);
+      }
+    }
+  }
+}
