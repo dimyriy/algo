ad36f9ec783b6ea12191e949fe65959f428aec38
Dmitrii Bogdanov <dimyriy.bogdanov@gmail.com>
2018-07-29 22:50:12 +0200

    - Add fibonacci calculator

---
+++ src/main/java/org/dimyriy/algorithms/generic/Fibonacci.java
@@ -0,0 +1,47 @@
+package org.dimyriy.algorithms.generic;
+
+import java.util.ArrayList;
+
+/**
+ * @author Dmitrii Bogdanov
+ * Created at 29.07.18
+ */
+@SuppressWarnings("SameParameterValue")
+class Fibonacci {
+  private final ArrayList<Integer> cache = new ArrayList<>(4);
+
+  Fibonacci() {
+    cache.add(0);
+    cache.add(1);
+  }
+
+  int fRecursive(final int n) {
+    if (n < 2) {
+      return n;
+    }
+    return fRecursive(n - 1) + fRecursive(n - 2);
+  }
+
+  int fIterative(final int n) {
+    if (n < 2) {
+      return n;
+    }
+    int current = 1, previous = 0;
+    for (int i = 1; i < n; i++) {
+      final int temp = current + previous;
+      previous = current;
+      current = temp;
+    }
+    return current;
+  }
+
+  int fMemoizationBased(final int n) {
+    if (cache.size() > n) {
+      return cache.get(n);
+    } else {
+      final int nth = fMemoizationBased(n - 1) + fMemoizationBased(n - 2);
+      cache.add(n, nth);
+      return nth;
+    }
+  }
+}
---
+++ src/test/java/org/dimyriy/algorithms/generic/FibonacciTest.java
@@ -0,0 +1,57 @@
+package org.dimyriy.algorithms.generic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Dmitrii Bogdanov
+ * Created at 29.07.18
+ */
+class FibonacciTest {
+  @Test
+  void recursiveMethodReturnsCorrectAnswer() {
+    Assertions.assertEquals(10946, new Fibonacci().fRecursive(21));
+  }
+
+  @Test
+  void iterativeMethodReturnsCorrectAnswer() {
+    Assertions.assertEquals(10946, new Fibonacci().fIterative(21));
+  }
+
+  @Test
+  void memoizationBasedMethodReturnsCorrectAnswer() {
+    Assertions.assertEquals(10946, new Fibonacci().fMemoizationBased(21));
+  }
+
+  @Test
+  void iterativeMethodReturnsCorrectFirstFibonacciNumber() {
+    Assertions.assertEquals(1, new Fibonacci().fIterative(1));
+  }
+
+  @Test
+  void iterativeMethodReturnsCorrectZerothFibonacciNumber() {
+    Assertions.assertEquals(0, new Fibonacci().fIterative(0));
+  }
+
+  @Test
+  void recursiveMethodReturnsCorrectFirstFibonacciNumber() {
+    Assertions.assertEquals(1, new Fibonacci().fRecursive(1));
+  }
+
+  @Test
+  void recursiveMethodReturnsCorrectZerothFibonacciNumber() {
+    Assertions.assertEquals(0, new Fibonacci().fRecursive(0));
+  }
+
+  @Test
+  void memoizationBasedMethodReturnsCorrectFirstFibonacciNumber() {
+    Assertions.assertEquals(1, new Fibonacci().fMemoizationBased(1));
+  }
+
+  @Test
+  void memoizationBasedMethodReturnsCorrectZerothFibonacciNumber() {
+    Assertions.assertEquals(0, new Fibonacci().fMemoizationBased(0));
+  }
+}
