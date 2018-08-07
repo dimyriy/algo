23e969e3a84bce6abec9d76ef1f0fa6d815498d0
Dmitrii Bogdanov <dimyriy.bogdanov@gmail.com>
2018-08-07 08:40:47 +0200

    - Add binomial heap skeleton

---
+++ src/main/java/org/dimyriy/datastructures/BinomialHeap.java
@@ -0,0 +1,16 @@
+package org.dimyriy.datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Dmitrii Bogdanov
+ * Created at 07.08.18
+ */
+public class BinomialHeap {
+  final List<Node> roots = new ArrayList<>();
+
+  private static class Node {
+    private int rank;
+  }
+}
